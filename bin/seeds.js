03f6adb7aea70b23dbc868ca24bf49fa6df99daa
Javier Neira <javier.neira@web.de>
2023-01-11 21:08:26 +0100

    Co-authored-by: Iryna Danchuk <Iryna1791@users.noreply.github.com>

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,9 +1,7 @@
-// seeds/drones.seed.js
 const mongoose = require("mongoose");
-const Recipe = require("../models/Recipe.model"); // the DroneModel will be used to create new drones in our DB
+const Recipe = require("../models/Recipe.model"); 
 
 
-// the array of drone objects to be created
 const recipes = [
     { cousine: "italian", 
      title: 'Spaghetti Genovese', 
@@ -69,9 +67,6 @@ const recipes = [
 
 ];
 
-// below is the exact same database connection setup as our main app 'db/index.js'
-// inside the .then() block we will seed our DB (Once the DB connection is established)
-
 
 const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project2";
@@ -80,18 +75,15 @@ mongoose
   .connect(MONGO_URI)
   .then(x => {
     console.log(`Connected to Mongo database: "${x.connections[0].name}"`);
- 
-    // Create new documents in the books collection
     return Recipe.create(recipes);
   })
+
   .then(recipesFromDB => {
     console.log(`Created ${recipesFromDB.length}recipes`);
  
-    // Once the documents are created, close the DB connection
     return mongoose.connection.close();
   })
   .then(() => {
-    // Once the DB connection is closed, print a message
     console.log('DB connection closed!');
   })
   .catch(err => {
